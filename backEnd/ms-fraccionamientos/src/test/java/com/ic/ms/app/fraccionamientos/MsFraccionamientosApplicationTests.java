950a526799b6f76d4f47fc02a78ee135ac37d00c
israel cid <israellcid@gmail.com>
2024-04-10 19:48:38 -0600

    Primer pull

---
+++ backEnd/ms-fraccionamientos/src/test/java/com/ic/ms/app/fraccionamientos/MsFraccionamientosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ic.ms.app.fraccionamientos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsFraccionamientosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
